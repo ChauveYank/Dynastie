87347ac7f452725f2b9290d14ed64ec30a762756
ChauveYank <mcgunnigetmarried@gmail.com>
2025-07-08 13:58:23 -0700

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "1048179556886159360"; // your league ID
+export const leagueID = "1180245112456769536"; // your league ID
 export const leagueName = "La Ligue de la Dynastie"; // your league name
 export const dues = 50; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
